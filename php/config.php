a4c231bbc2d32aeab75530769eef51a7fcfadfd7
michelwillen <122614013+michelwillen@users.noreply.github.com>
2023-01-13 13:41:43 +0100

    Delete config.php

--- php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host = "localhost";
-$user = "xxxxx";
-$password = "xxxxx";
-$dbname = "xxxxx";
-
-$pdo = new PDO('mysql:host='. $host . '; dbname=' . $dbname . ';charset=utf8', $user, $password);
-$pdo->exec("set names utf8");
